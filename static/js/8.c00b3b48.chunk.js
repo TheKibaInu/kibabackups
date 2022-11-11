2caf369a60476e54ea36f74d5d0884c321884e80
nutz <nutz>
2022-11-11 06:11:25 -0600

    Updates

--- static/js/8.c00b3b48.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@kiba/interface"]=this["webpackJsonp@kiba/interface"]||[]).push([[8],{1578:function(n,i){},1579:function(n,i){},1804:function(n,i){}}]);
-//# sourceMappingURL=8.c00b3b48.chunk.js.map
