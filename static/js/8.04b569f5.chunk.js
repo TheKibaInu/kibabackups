0b4fb8b74a5ea7bf1ea6afbd8e9060a042b2f408
Majgic <jeff@majgic.com>
2022-11-29 10:36:14 -0500

    Updates

---
+++ static/js/8.04b569f5.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@kiba/interface"]=this["webpackJsonp@kiba/interface"]||[]).push([[8],{1595:function(n,i){},1596:function(n,i){},1820:function(n,i){}}]);
+//# sourceMappingURL=8.04b569f5.chunk.js.map
